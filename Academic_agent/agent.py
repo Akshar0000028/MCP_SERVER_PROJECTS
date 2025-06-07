7cf8a8c874389e2e16007c3a127f20b2575ad983
Akshar0000028 <167672957+Akshar0000028@users.noreply.github.com>
2025-06-07 11:19:12 +0530

    Update agent.py

--- Academic_agent/agent.py
+++ Academic_agent/agent.py
@@ -14,7 +14,7 @@ class ResearchAgent:
     def init_llm(self):
         return ChatNVIDIA(
             model="meta/llama3-70b-instruct",
-            nvidia_api_key="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",
+            nvidia_api_key="NVIDIA_API_KEY",
             temperature=0.3
         )
 
